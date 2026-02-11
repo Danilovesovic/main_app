3decf02218df45a91f793fe47e23240170f6dfd0
Danilovesovic <vesovicdanilo@gmail.com>
2026-02-11 18:39:17 +0100

    Init commit

---
+++ router/admin-routes.js
@@ -0,0 +1,15 @@
+const TaskController = require('../controllers/admin/TaskController.js');
+const express = require('express');
+const router = express.Router();
+
+router.get('/dashboard', require('../controllers/admin/DashboardController.js'))
+router.get('/task', TaskController.index)
+
+router.get('/task/create',TaskController.create)
+
+router.post('/task', TaskController.store)
+
+
+router.delete('/task/:id', TaskController.destroy)
+
+module.exports = router;
