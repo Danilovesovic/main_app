08db395aa942130fbbd4c1d97041aa579374c2d6
Stefan Milanovic <stefan1811991@gmail.com>
2026-02-16 18:39:56 +0100

    feature/stefan.milanovic-messages: - FE // minor refactoring

--- controllers/admin/MessagesController.js
+++ controllers/admin/MessagesController.js
@@ -1,10 +1,10 @@
 const User = require('../../models/User');
 
-const Messages = async (req, res) => {
+const MessagesController = async (req, res) => {
 	const sessionUser = req.session.user;
 	const users = await User.find({ _id: { $ne: sessionUser._id } }).select('_id').select('username');
 
 	res.render('admin/messages', { title: 'Messages', user: sessionUser, users });
 };
 
-module.exports = Messages;
+module.exports = MessagesController;
